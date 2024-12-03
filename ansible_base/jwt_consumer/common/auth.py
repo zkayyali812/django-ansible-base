zkayyali812/django-ansible-base

commit 9f7c3b3ded544e5df338a5d3413e3beee788fb15

Fix linters and tests for new service_id key

ansible_base/jwt_consumer/common/auth.py
@@ -115,7 +115,10 @@ class JWTCommonAuth:
             # Either the user wasn't cached or the requested user was not in the DB so we need to make a new one
             try:
                 resource = Resource.create_resource(
-                    ResourceType.objects.get(name="shared.user"), resource_data=self.token["user_data"], ansible_id=self.token["sub"], service_id=self.token["service_id"]
+                    ResourceType.objects.get(name="shared.user"),
+                    resource_data=self.token["user_data"],
+                    ansible_id=self.token["sub"],
+                    service_id=self.token["service_id"],
                 )
                 self.user = resource.content_object
                 logger.info(f"New user {self.user.username} created from JWT auth")

test_app/tests/conftest.py
@@ -26,6 +26,7 @@ from ansible_base.lib.testing.util import copy_fixture, delete_authenticator
 from ansible_base.oauth2_provider.fixtures import *  # noqa: F403, F401
 from ansible_base.rbac import permission_registry
 from ansible_base.rbac.models import RoleDefinition
+from ansible_base.resource_registry.models.service_identifier import service_id
 from test_app import models
 
 
@@ -517,6 +518,7 @@ def jwt_token(test_encryption_private_key):
                 "exp": int(expiration_date.timestamp()),
                 "aud": "ansible-services",
                 "sub": "1e3de989-5286-48a6-83d4-5de9a6618ffd",
+                "service_id": service_id(),
                 "user_data": {
                     "username": "john.westcott.iv",
                     "first_name": "john",

commit 832e4a846ca9b0ac6c35867c0e628605f66ae5d9

Do not error when missing some JWT item (#659)
Scary exception from controller, caused by
https://github.com/ansible/django-ansible-base/pull/612 without
corresponding JWT data

It should be there in the JWT, but let's just not take chances since the
reasons for that being there or not are so hard for us to pin down at
this moment.

ansible_base/jwt_consumer/common/auth.py
@@ -113,13 +113,14 @@ class JWTCommonAuth:
 
         if not self.user:
             # Either the user wasn't cached or the requested user was not in the DB so we need to make a new one
+            resource_kwargs = {}
+            for resource_key, token_key in (('resource_data', 'user_data'), ('ansible_id', 'sub'), ('service_id', 'service_id')):
+                if token_key not in self.token:
+                    logger.warning(f'Missing {token_key} in JWT data, omitting {resource_key} from local resource entry')
+                else:
+                    resource_kwargs[resource_key] = self.token[token_key]
             try:
-                resource = Resource.create_resource(
-                    ResourceType.objects.get(name="shared.user"),
-                    resource_data=self.token["user_data"],
-                    ansible_id=self.token["sub"],
-                    service_id=self.token["service_id"],
-                )
+                resource = Resource.create_resource(ResourceType.objects.get(name="shared.user"), **resource_kwargs)
                 self.user = resource.content_object
                 logger.info(f"New user {self.user.username} created from JWT auth")
             except IntegrityError as exc: